4f9125db36b9a51258f9183defac29dbd5078228
Henrique <henrique.hfr2006@gmail.com>
2022-01-15 02:08:51 -0300

    main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var typed = new Typing(".typing", {
-    strings: ["Web Designer", "Web Developer"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true,
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
 })
